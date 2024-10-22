cdc8217068f2e8f306d31b84599822aafac358f6
turkialfaend <2300016141@webmail.uad.ac.id>
2024-10-22 16:20:07 +0700

    attemptn 1 - 14.15

--- web-technology-mid-test-turkialfaend-main/script.js
+++ web-technology-mid-test-turkialfaend-main/script.js
@@ -1,6 +1,6 @@
 // TODO: Implement the password generation logic based on user input
 
-const generatePassword = (length, options) => {
+export const generatePassword = (length, options) => {
     // Character sets for password generation
     const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const lowercase = "abcdefghijklmnopqrstuvwxyz";
